baf5eb19e85a6b9206e7134e522f00279be17fbd
Capibarson <cabreraytpl@gmail.com>
2023-12-11 14:41:21 +0100

    Add project files.

---
+++ 11Grudnia/Program.cs
@@ -0,0 +1,13 @@
+﻿string path = Console.ReadLine();
+if(File.Exists(path))
+{
+
+} else
+{
+    Console.Write("Path" + "\n nie istnieje.")
+}
+/* 
+-Klasa File, Directory, Path
+- Operacje na plikach (czy istnieje, odczyt, zapis, przenieś, usuń)
+-Operacje na katalogach(czy istnieje, stwórz, usuń, lista wszystkich plików oraz katalogów)
+-Operacje na strumieniach(StreamReader, StreamWriter) */
